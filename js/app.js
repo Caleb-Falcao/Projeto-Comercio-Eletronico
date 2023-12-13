8be6b8ec72c24731222cec383cd5a451b01dd684
Caleb-Falcao <calebalves2011@hotmail.com>
2023-12-13 09:55:48 -0300

    alteracao na pagina

--- js/app.js
+++ js/app.js
@@ -7,3 +7,43 @@ document.addEventListener("DOMContentLoaded", function () {
     searchForm.classList.toggle("active");
   });
 });
+
+
+function search() {
+  let input = document.getElementById("searchbar").value
+  input = input.toLowerCase()
+  let x = document.getElementsByClassName('produto')
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = 'none'
+    } else {
+      x[i].style.display = 'list-item'
+    }
+  }
+
+}
+/*ocultar div detalhes da compra */
+$('.det-compra').hide();
+
+//Botão btn info ao ser clicado
+$('.btn-det').on('click', function () {
+  $(this).hide();
+  $('.pag-inicial').hide();
+  $('.det-compra').show();
+
+});
+
+$(document).ready(function() {
+  // Quando o botão com a classe 'btn-success' for clicado
+  $('.btn-comprar').click(function() {
+    // Exibe um alerta para o usuário
+    alert('Produto comprado com sucesso!');
+  });
+});
+
+/*reiniciar pagina */
+function reloadPage() {
+  // Recarrega a página atual
+  window.location.reload();
+}
